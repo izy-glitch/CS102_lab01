cc3252aa2efd4edd259dbaeebbe6bd11542bea78
izy-glitch <85749680+izy-glitch@users.noreply.github.com>
2021-06-19 23:58:01 +0300

    Update Fibonacci.java

--- 01_Lab01_Yılmaz_İlayda/src/Fibonacci.java
+++ 01_Lab01_Yılmaz_İlayda/src/Fibonacci.java
@@ -4,16 +4,13 @@ public class Fibonacci {
         IntBag fibonacci = new IntBag();
         int first = 0;
         int second = 1;
-        int nextNumber;
 
-        fibonacci.addValue( first );
-        fibonacci.addValue( second );
+        fibonacci.addIndexValue( first, 0 );
+        fibonacci.addIndexValue( second, 1 );
 
-        for ( int i = 0; fibonacci.numberOfValues() <= 38; i++){
-
-            nextNumber = fibonacci.getTheValue( i ) + fibonacci.getTheValue( i + 1 );
-            fibonacci.addValue( nextNumber );
-
+        for ( int i = 0; i < 38; i++){
+            fibonacci.addIndexValue(fibonacci.getTheValue( i ) + fibonacci.getTheValue( i + 1 ),
+                    i + 2 );
         }
 
         System.out.println( fibonacci );
